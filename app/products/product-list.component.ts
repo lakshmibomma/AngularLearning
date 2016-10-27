5cce63970b8695e9f52bbafd0c5d60af3447967f
Lakshmi Bomma <lbomma@riis.com>
2016-10-27 13:26:29 -0400

    services and dependency injection added

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import {IProduct} from './product';
+import {ProductService} from './product.service'
+
 
 @Component({
     selector: 'pm-products',
@@ -13,28 +15,14 @@ export class ProductListComponent  implements OnInit{
     imageMargin: number = 2;
     showImage:boolean = false;
     listFilter:string = 'cart';
-     products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        }
-    ];
+    private _productService:ProductService;
+     products: IProduct[] = [];
+
+    constructor(productService:ProductService)
+    {
+        this._productService = productService;
+    }
+
     toggleImage():void
     {
         this.showImage = !this.showImage;
@@ -42,6 +30,7 @@ export class ProductListComponent  implements OnInit{
     
     ngOnInit(): void {
         console.log('In OnInit');
+        this.products = this._productService.getProducts();
     }
 
     onRatingClicked(message:string):void
